5f77a43916bdaf16cf75a73bd06c9050bd116812
Alex Beauchamp <abeauchamp@teksynrgy.com>
2013-07-13 16:17:30 -0400

    Study drill 1 chanegs for ex3

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -1,23 +1,35 @@
+# This line will print the following string
 print "I will now count my chickens:"
 
+# This line will print the total num of hens based on the math
 print "Hens", 25 + 30 / 6
+# This line will print the total num of egss based on the math
 print "Roosters", 100 - 25 * 3 % 4
 
+# THis line will print the following string
 print "Now I will count the eggs:"
 
+# This line prints the result of the math expression
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# This line prints the following string
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# This line prints the result of the expression from the above string
 print 3 + 2 < 5 - 7
 
+# This line prints a string and the result of an expression
 print "What is 3 + 2?", 3 + 2
+# This line prints a string and the result of an expression
 print "what is 5 - 7?", 5 - 7
 
+# This line prints the following string
 print "Oh, that's why it's False."
 
+# This line prints the following string
 print "How about some more."
 
+# The following 3 print statements, print a string and then the result of an expression
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
