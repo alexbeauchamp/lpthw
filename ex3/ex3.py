cd740263b0f0fe5b63fb76148c03b8ff905d4d63
Alex Beauchamp <abeauchamp@teksynrgy.com>
2013-07-13 16:22:53 -0400

    Study drill 4 changes for ex3

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -2,26 +2,26 @@
 print "I will now count my chickens:"
 
 # This line will print the total num of hens based on the math
-print "Hens", 25 + 30 / 6
+print "Hens", 25.0 + 30.0 / 6.0
 # This line will print the total num of egss based on the math
-print "Roosters", 100 - 25 * 3 % 4
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
 # THis line will print the following string
 print "Now I will count the eggs:"
 
 # This line prints the result of the math expression
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # This line prints the following string
 print "Is it true that 3 + 2 < 5 - 7?"
 
 # This line prints the result of the expression from the above string
-print 3 + 2 < 5 - 7
+print 3.0 + 2.0 < 5.0 - 7.0
 
 # This line prints a string and the result of an expression
-print "What is 3 + 2?", 3 + 2
+print "What is 3 + 2?", 3.0 + 2.0
 # This line prints a string and the result of an expression
-print "what is 5 - 7?", 5 - 7
+print "what is 5 - 7?", 5.0 - 7.0
 
 # This line prints the following string
 print "Oh, that's why it's False."
@@ -30,6 +30,6 @@ print "Oh, that's why it's False."
 print "How about some more."
 
 # The following 3 print statements, print a string and then the result of an expression
-print "Is it greater?", 5 > -2
-print "Is it greater or equal?", 5 >= -2
-print "Is it less or equal?", 5 <= -2
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
