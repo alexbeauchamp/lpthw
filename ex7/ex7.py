adcfa31c91b44a98e1f1568b27a7b01161ad47d8
Alex Beauchamp <abeauchamp@teksynrgy.com>
2013-07-24 14:38:04 -0400

    Updated for SD 1

--- ex7/ex7.py
+++ ex7/ex7.py
@@ -1,8 +1,13 @@
+# Prints to screen the below string
 print "Mary had a little lamb."
+# Prints to screen the below string with included formatter
 print "Its fleece was white as %s." % 'snow'
+# Prints to screen the below string
 print "And everywhee that Mary went."
-print "." * 10 # Prints . 10 times
+#Prints to screen . 10 times
+print "." * 10
 
+# Below statements assign values to variables end1 thorugh end12
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -16,5 +21,8 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
+# Creates a prints a string with variables end1 through end12
+# The comma at the end of the first print statments joins the two string on
+# one line
 print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
